68a600144243b0f9d28098c69a4c6e822de9de4e
Eric Zou <55918461+St0nkyCheeseMan@users.noreply.github.com>
2021-11-07 12:00:12 -0500

    Added index.js and config.js
    Title

---
+++ config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+var fs = require('fs');
+exports.config = {
+    "token": "OTA2OTMwMjc5NDg5MzcyMTYx.YYfypA.wNnbRIfAU6mOsPUd3IQW86sZAQc",
+    "prefix": "!",
+    "commands": cfiles,
+    "events": efiles,
+    "clientID": "885542468693676054",
+    "guildID": "886326356072337438"
+};
