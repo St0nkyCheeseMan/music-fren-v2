9c0743a90cac5bc47cb8658ca1367568909e5fcf
Eric Zou <55918461+St0nkyCheeseMan@users.noreply.github.com>
2021-11-07 20:39:16 -0500

    index.js now parses channel for links and has a new avatar

--- index.js
+++ index.js
@@ -7,24 +7,45 @@ const Bot = new Discord.Client({intents: myIntents});
 
 const fs = require('fs');
 
-
-async function init(guild){
-    await guild.channels.fetch();
-    await Bot.application?.fetch();
-    // here we do the shit, look for channel rec and do stuff
+async function fetchAll(channel){
+    let collection = new Discord.Collection();
+    let options = {};
+    //channel.send("Starting compilation process");
+    let lastId = await channel.messages.fetch({limit : 1}).id;
+    while(true){
+        options.before = lastId;
+        let messages = await channel.messages.fetch(options);
+        collection = collection.concat(messages);
+        if(!messages.last()){
+            break;
+        }
+        lastId = messages.last().id;
+    }
+    return collection;
 }
 
 Bot.once("ready", async () => {
     console.log("This bot is online!"); //standard protocol when starting up the bot
     Bot.user.setPresence({ activities: [{ name: 'music videos', type:'WATCHING' }], status: 'online' });
-    Bot.user?.setUsername("music fren");
-    
-    Bot.guilds.fetch().then(() => {
-        Bot.guilds.cache.forEach(async (guild) => {
-            console.log(guild.name);
-            await init(guild);
-        })
-    })
-    })
+    Bot.user.setUsername("Playlist Plus");
+    Bot.user.setAvatar("download.png");
+    channel = await Bot.channels.fetch("575857030770851847");
+    messages = await fetchAll(channel);
+    linkmsgs = messages.filter(msg => msg.content.includes('youtu'));
+    linkmsgs = linkmsgs.filter(msg => msg.content.includes('https://'));
+    rawmsgs = linkmsgs.map(msg => msg.content);
+    ytlinks = []
+    var re = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
+    for(var i = 0; i < rawmsgs.length; i++){
+        matches = await rawmsgs[i].match(re);
+        try{
+            ytlinks.push(matches[0]);
+        }
+        catch(error){
+            //pass
+        }
+    }
+    //console.log(ytlinks);
+})
 
 Bot.login(Config.config.token); //logs in using token in config.config (not accessible to you)
